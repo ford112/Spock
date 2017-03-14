175bb3d8f58dabfbd959cfcaeccc0059ff6ef2b4
Bobby Kain <robertkain@Roberts-MacBook-Pro.local>
2017-03-13 22:25:25 -0700

    nim

--- Nim.java
+++ Nim.java
@@ -9,9 +9,7 @@ public class Nim
   {
     String row = "";
     for (int i = 0; i < num; ++i)
-    {
       row += "X ";
-    }
     return row;
   }
 
@@ -19,11 +17,34 @@ public class Nim
   {
     String board = "";
     for (int i = 0; i < 4; ++i)
-    {
       board += getRow(heaps[i]) + "\n";
-    }
     return board;
   }
 
-  
+  public void updateHeap(int numTaken, int heap)
+  {
+    if (numTaken <= heaps[heap])
+      heaps[heap] -= numTaken;
+  }
+
+  public boolean checkGameover()
+  {
+    int gameOver = false;
+
+    int numEmpty = 0;
+    int numOne = 0;
+
+    for (int i = 0; i < 4; ++i)
+    {
+      if (heaps[i] == 0)
+        numOne++;
+      else if (heaps[i] == 1)
+        numOne++;
+    }
+
+    if (numEmpty == 3 && numOne == 1)
+      gameOver = true;
+
+    return gameOver;
+  }
 }
