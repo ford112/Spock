580ffbaef7b1ec18dcaa6018460b1927a89f6bcc
Akash Arora <arora110@mail.chapman.edu>
2017-04-24 13:37:09 -0700

    update2

--- Spock.java
+++ Spock.java
@@ -4,35 +4,27 @@ import java.util.*;
 
 public class Spock
 {
-<<<<<<< HEAD
-
-=======
->>>>>>> 8cf04c352945c80675bc4541bac18555469c5601
   Spock()
  {}
 
-<<<<<<< HEAD
   // return a visual of the stacks
   //public String getBoard()
   //{
   //
   //}
-=======
+
   // check win conditions
   public void decideWinner(String input1, String input2)
   {
->>>>>>> 8cf04c352945c80675bc4541bac18555469c5601
+
 
     System.out.println("HI");
   }
 
-<<<<<<< HEAD
+
   // check win conditions
   public boolean isOver()
   {
       return false;
   }
-=======
-
->>>>>>> 8cf04c352945c80675bc4541bac18555469c5601
 }
